d32b5d209c550a7c150508b15f5b486baf5662ef
Tiree Jackson <tiree_jackson@yahoo.com>
2020-09-08 16:05:29 -0400

    finish the assessment flake8 didnt pass

--- morse.py
+++ morse.py
@@ -9,24 +9,39 @@ Pause between dots and dashes in a character – is 1 time unit long.
 Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
 """
-__author__ = '???'
+__author__ = 'tiree With the study group help Mike the coach great help.'
 
 from morse_dict import MORSE_2_ASCII
 
 
 def decode_bits(bits):
     # your code here
-    return
+    bits = bits.strip('0')
+    unit = min([len(element)
+                for element in bits.split("1") + bits.split("0") if element])
+    print("unit", unit)
+    morse_code = bits.replace("0000000" * unit, "   ").replace("111" * unit, "-").replace(
+        "1" * unit, ".").replace("000" * unit, " ").replace("0" * unit, "")
+
+    return morse_code
 
 
 def decode_morse(morse):
     # your code here
-    return
+    morse_code = []
+    code_split_words = morse.strip().split("   ")
+    for word in code_split_words:
+        word_split_chars = word.split(' ')
+        new_word = []
+        for char in word_split_chars:
+            new_word.append(MORSE_2_ASCII[char])
+        morse_code.append(''.join(new_word))
+    return ' '.join(morse_code)
 
 
 if __name__ == '__main__':
     hey_jude_morse = ".... . -.--   .--- ..- -.. ."
-    hey_jude_bits = "11001100110011000000110000001111110011001111110011111100000000000000000000011001111110011111100111111000000110011001111110000001111110011001100000011" # noqa
+    hey_jude_bits = "11001100110011000000110000001111110011001111110011111100000000000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"  # noqa
 
     # Be sure to run all included unit tests, not just this one.
     print("Morse Code decoder test")
